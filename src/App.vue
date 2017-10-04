b6ff6f5d4427f385935bd60208d128870016cc20
lwio <443474713@qq.com>
2017-10-04 14:57:38 +0800

    a new project

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,11 @@
 </template>
 
 <script>
-import MusicPlayer from '@/components/MusicPlayer'
-import {mapActions,mapGetters} from 'vuex'//使用状态管理里的mapActions来传递方法，mapGetters来展示数据
+import {mapActions,mapGetters} from 'vuex'
 export default {
   name: 'app',
   components:{
-    MusicPlayer
+    
   },
   data() {
     return {
@@ -28,17 +27,9 @@ export default {
     }
   },
   methods: {
-    ...mapActions([
-        'setPlayerDOM',
-        'syncCurrentTime',//循环播放（自动下一曲）
-    ]),
-    change() {
-      this.dialogTableVisible = true
-      console.log("fuck")
-    }
+
   },
   mounted() {
-    this.$store.dispatch('setPlayerDOM',this.$refs.audio);
   },
 
 }
@@ -46,23 +37,8 @@ export default {
 
 <style lang="scss">
 @import "../static/style/public.scss";
-@import './styles/index.scss'; // 全局自定义的css样式`
+@import './common/style/index.scss'; // 全局自定义的css样式`
 #app {
-  display: flex;
-  flex-direction: column;
-  .header {
-
-  }
-  .body {
-    display: flex;
-    flex: 1;
-    & > div:not(#leftnav) {
-      flex: 1;
-      overflow: auto;
-    }
-  }
-  .footer {
-
-  }
+  
 }
 </style>
